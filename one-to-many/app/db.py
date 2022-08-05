c9cfd9c40fefa248890218bc8e2dc3aaa5591665
Ben Botsford <ben.botsford@flatironschool.com>
2022-08-05 15:01:14 -0500

    through seeding

--- one-to-many/app/db.py
+++ one-to-many/app/db.py
@@ -4,7 +4,8 @@ import sys
 sys.path.append(os.getcwd)
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///one_to_many.db')
@@ -19,3 +20,13 @@ class Game(Base):
     game_genre = Column(String())
     game_platform = Column(String())
     game_price = Column(Integer())
+
+    reviews = relationship('Review', backref=backref('game'))
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    review_id = Column(Integer(), primary_key=True)
+    review_score = Column(Integer())
+    review_comment = Column(String())
+    game_id = Column(Integer(), ForeignKey('games.game_id'))
